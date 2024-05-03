de22abb279b135445bee3268cf9aeacedae97048
K1ra-Python <makismka.guchenko@mail.ru>
2024-05-03 20:36:35 +0300

    #

--- pages/authorization.vue
+++ pages/authorization.vue
@@ -25,6 +25,17 @@ export default defineComponent({
         // Вместо деструктуризации $router используйте контекст напрямую для получения маршрутизатора
         const nuxtApp = useNuxtApp();
         const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/auth.user
+                const uid = user.uid;
+                // ...
+            } else {
+                // User is signed out
+                // ...
+            }
+        });
         const router = useRouter()
         const state = reactive({
             userAuthData: {
