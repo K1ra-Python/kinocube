fa7ccf6f7d21accec9ff2dea62cd313e927c2706
K1ra-Python <makismka.guchenko@mail.ru>
2024-05-03 20:15:12 +0300

    profile almost done

---
+++ components/profileStuff/changeGeners.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="iconAndText">
+        <div class="icon"> <img src="~/assets/arrow-swap-horizontal.svg"></div>
+        <div class="text">Поменять жанры</div>
+    </div>
+</template>
+<script>
+
+</script>
+<style lang="scss">
+.iconAndText {
+    display: flex;
+    justify-content: left;
+    gap: 15px;
+    align-items: center;
+
+    .icon {
+        padding-top: 4%;
+    }
+
+    .text {
+        font-weight: 400;
+        font-size: 23px;
+        color: #fff;
+    }
+}
+</style>
---
+++ components/profileStuff/liked.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="iconAndText">
+        <div class="icon">  <img src="~/assets/heart_transparent.svg"></div>
+        <div class="text">Понравившейся</div>
+    </div>
+</template>
+<script>
+
+</script>
+<style></style>
