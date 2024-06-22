07ca43b86502aaff57fb25a546e411b5161a29dd
K1ra <70842864+K1ra-Python@users.noreply.github.com>
2024-06-22 15:26:40 +0300

    Update firebase.js

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -7,13 +7,7 @@ const firebaseConfig = {
     // ... ваш Firebase config 
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-    apiKey: "AIzaSyBitsjuGgHcwkh347w8F8YdmdKrL0zRFo8",
-    authDomain: "kinocube-8ac50.firebaseapp.com",
-    projectId: "kinocube-8ac50",
-    storageBucket: "kinocube-8ac50.appspot.com",
-    messagingSenderId: "1042756316645",
-    appId: "1:1042756316645:web:827b3da0e20f37349c130c",
-    measurementId: "G-PWZRSMGCQR"
+    
 
 }
 
